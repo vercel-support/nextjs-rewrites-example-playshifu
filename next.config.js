4648c11c3e1a408530977996b421b66c68e977fd
William Li <email@williamli.dev>
2021-05-20 17:07:52 +0800

    added rewrite to https://blog.playshifu.com/

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // These rewrites are checked after headers/redirects
+        // and before all files including _next/public files which
+        // allows overriding page files
+        {
+          source: '/blog/:slug*',
+          destination: 'https://blog.playshifu.com/blog/:slug*/'
+        },
+      ]
+    }
+  },
+}
